4e5774fd027d7aa61ea7bfd5bfedf82dcccdd68e
xsthunder <xsthunder@outlook.com>
2020-03-17 15:17:40 +0800

    add test

--- tests/test_cli.sh
+++ tests/test_cli.sh
@@ -10,3 +10,4 @@ python ../main.py 5tKOV0KqPlg --to_json=True
 python ../main.py 5tKOV0KqPlg --translation 'ja' --output_file='test.ja.txt'
 python ../main.py 5tKOV0KqPlg --translation 'ja' --translation 'jp'  --output_file='test.ja.json'
 python ../main.py 5tKOV0KqPlg --translation False --output_file='test.none.json'
+python ../main.py "https://www.youtube.com/watch?v=EozTm6ZVf1U" --caption_num=1
--- tests/test_main.py
+++ tests/test_main.py
@@ -149,7 +149,7 @@ import sys
 from functools import partial
 import json
 import re
-def main(videoID, output_file=None, save_to_file=True, translation='zh-Hans', to_json=False):
+def main(videoID, output_file=None, save_to_file=True, translation='zh-Hans', to_json=False, caption_num=0):
     """
     download youtube closed caption(subtitles) by videoID
 
@@ -166,6 +166,7 @@ def main(videoID, output_file=None, save_to_file=True, translation='zh-Hans', to
     save_to_file: bool, default to True, True or False
     translation: bool or string, default to 'zh-Hans' for simplified Chinese, False or lang code, see ./lang_code.json for full list
     to_json: bool, default to False, export caption to json
+    caption_num: number, default to 0, choose the caption
 
     """
 
@@ -175,19 +176,20 @@ def main(videoID, output_file=None, save_to_file=True, translation='zh-Hans', to
 
     info = partial(print, "INFO: ")
 
-    info("available caption(s) are")
+    info("available caption(s)(can be chosed by caption_num):")
 
-    for caption in captionTracks:
-        info(caption['name']['simpleText'], '')
+    for i, caption in enumerate(captionTracks):
+        info(f"\t{i}.", caption['name']['simpleText'])
 
-    info('using',captionTracks[0]['name']['simpleText'] )
+    caption = captionTracks[caption_num]
+    info('using',f"{caption_num}.", caption['name']['simpleText'] )
 
-    baseUrl = captionTracks[0]['baseUrl']
+    baseUrl = caption['baseUrl']
     transcript = requests.get(baseUrl)
     subtitle = parseTranscript(transcript)
 
     if translation:
-        baseUrl = captionTracks[0]['baseUrl'] + '&tlang=' + translation
+        baseUrl = caption['baseUrl'] + '&tlang=' + translation
         transcript = requests.get(baseUrl)
         subtitle_cn = parseTranscript(transcript)
         for sub, cn in zip(subtitle, subtitle_cn):
@@ -226,3 +228,5 @@ if __name__ == '__main__':
     else :
         set_fire(main)
 fire_main = partial(set_fire, main)
+
+main('https://www.youtube.com/watch?v=EozTm6ZVf1U', caption_num=1)
