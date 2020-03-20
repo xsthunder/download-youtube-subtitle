5a5dd6650d22dec5e2f1634f22e04c68c1305af4
xsthunder <xsthunder@outlook.com>
2020-03-20 16:45:56 +0800

    Update rebuild.cmd

--- rebuild.cmd
+++ rebuild.cmd
@@ -1,3 +1,3 @@
 pip uninstall download-youtube-subtitle -y
 python setup.py sdist bdist_wheel
-pip install .\dist\download_youtube_subtitle-0.0.9-py3-none-any.whl
+pip install .\dist\download_youtube_subtitle-0.1.0-py3-none-any.whl
